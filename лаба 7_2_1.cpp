b24a5966d2f40ee2b18caf9ee62a9971d4053225
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-20 19:02:54 +0200

    first master commit

---
+++ лаба 7_2_1.cpp
@@ -0,0 +1,155 @@
+﻿// Lab_7_2_1.cpp
+// < Копейки Христини >
+// Лабораторна робота № 7.2.1)
+// Опрацювання багатовимірних масивів ітераційними способами.
+// Варіант 8
+
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+
+using namespace std;
+
+void Create(int** a, const int k, const int n, const int Low, const int High);
+void Print(int** a, const int k, const int n);
+bool SearchMaxEven(int** a, const int k, const int n, int& minEven);
+bool SearchMinEven(int** a, const int k, const int n, int& minEven);
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	int Low = -12;
+	int High = 37;
+	int k,n;
+	cout << "k = "; cin >> k;
+	cout << "n = "; cin >> n;
+
+	int** a = new int* [k];
+	for (int i = 0; i < k; i++)
+		a[i] = new int[k];
+	int** a2 = new int* [n];
+	for (int i = 0; i < n; i++)
+		a[i] = new int[n];
+
+	Create(a, k, n, Low, High);
+	Print(a, k, n);
+
+	int minEven;
+	if (SearchMinEven(a, k, n, minEven))
+		cout << "min even = " << minEven << endl;
+	else
+		cout << "there are no even elements" << endl;
+	int maxEven;
+	if (SearchMaxEven(a, k, n, maxEven))
+		cout << "max even = " << maxEven << endl;
+	else
+		cout << "there are no even elements" << endl;
+
+	for (int i = 0; i < k; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+
+	for (int i = 0; i < n; i++)
+		delete[] a[i];
+	delete[] a;
+	return 0;
+}
+void Create(int** a, const int k, const int n, const int Low, const int High)
+{
+	for (int i = 0; i < k; i++)
+		for (int j = 0; j < k; j++)
+			a[i][j] = Low + rand() % (High - Low + 1);
+	
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				a[i][j] = Low + rand() % (High - Low + 1);
+}
+void Print(int** a, const int k, const int n)
+{
+	cout << endl;
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << setw(4) << a[i][j];
+		cout << endl;
+	}
+	cout << endl;
+}
+bool SearchMaxEven(int** a, const int k, const int n, int& minEven)
+{
+	return false;
+}
+bool SearchMinEven(int** a, const int k, const int n, int& minEven)
+{
+	return false;
+}
+bool SearchMinEven(int** a, const int n, int& minEven)
+{
+	int j, k = -1;
+	for (int i = 0; i < n; i++)
+	{
+		j = n - 1 - i;
+		if (a[i][j] % 2 == 0)
+		{
+			k = i;
+			minEven = a[i][j];
+			break;
+		}
+	}
+
+	if (k == -1)
+		return false;
+	for (int i = k + 1; i < n; i++)
+	{
+		j = n - 1 - i;
+		if (a[i][j] % 2 == 0 && a[i][j] < minEven)
+		{
+			minEven = a[i][j];
+		}
+	}
+	return true;
+}
+bool SearchMaxEven(int** a, const int n, int& maxEven)
+{
+	int j, k = -1;
+	for (int i = 0; i < n; i++)
+	{
+		j = k - 1 - i;
+		if (a[i][j] % 2 == 0)
+		{
+			k = i;
+			maxEven = a[i][j];
+			break;
+		}
+	}
+
+	if (k == -1)
+		return false;
+	for (int i = k + 1; i < n; i++)
+	{
+		j = n - 1 - i;
+		if (a[i][j] % 2 == 0 && a[i][j] < maxEven)
+		{
+			maxEven = a[i][j];
+		}
+	}
+	return true;
+}
+
+
+
+
+
+
+
+
---
+++ юніт тест 7.2.1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 7_2_1/лаба 7_2_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест721
+{
+	TEST_CLASS(юніттест721)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int s[1] = { 1 };
+			int** a = new int* [1];
+			for (int i = 0; i < 1; i++)
+				a[i] = new int[1];
+			Create(a, 1, 1,1,1);
+			int minEven = 1;
+			int maxEven = 1;
+			int t = SearchMinEven(a, 1, minEven);
+			int t2 = SearchMaxEven(a, 1, maxEven);
+			Assert::AreEqual(t, 0);
+		}
+	};
+}
